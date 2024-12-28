207198faeef8c1fbb7f523068866ab7b175128e5
Alexandr-cloud <kosyakov-a@mail.ru>
2024-12-28 16:35:43 +0300

    Практическая задача 2.2.2 MVC-приложение.

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -6,31 +6,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.models.Car;
-import web.service.CarServiceImpl;
-
-import java.util.ArrayList;
+import web.service.CarService;
 import java.util.List;
 
 @Controller
 public class CarController {
 
-    private CarServiceImpl carService;
+    private CarService carService;
 
     @Autowired
-    public CarController(CarServiceImpl carService) {
+    public CarController(CarService carService) {
         this.carService = carService;
     }
 
     @GetMapping("/cars")
     public String cars(@RequestParam(value = "count", required = false, defaultValue = "0") int count, Model model) {
-        List<Car> allCars = new ArrayList<>();
-        allCars.add(new Car("Toyota", "Silver", 2015));
-        allCars.add(new Car("Honda", "Red", 2020));
-        allCars.add(new Car("Ford", "Blue", 2017));
-        allCars.add(new Car("BMW", "Black", 2023));
-        allCars.add(new Car("Mercedes", "White", 2021));
-
-        List<Car> cars = carService.getCars(allCars, count);
+        List<Car> cars = carService.getCars(count);
         model.addAttribute("cars", cars);
         return "cars";
     }
